bcdc8125113def3c2f88934760c74440c392cb37
Sarahi Vazquez <sara011.svz@gmail.com>
2022-06-22 01:20:30 -0500

    estructura/disenio hecho

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,19 +1,22 @@
+import { Toolbar } from '@mui/material';
 import { Box } from '@mui/system';
+import { NavBar, SideBar } from '../components';
 
-const drawerWidth = 240;
+const drawerWidth = 280;
 
 export const JournalLayout = ({children}) => {
     return (
         <Box sx={{ display: 'flex' }} >
-            {/**Navbar  drawerWidth */}
 
-            {/**Sidebar  drawerWidth */}
+            <NavBar drawerWidth={ drawerWidth } />
+
+            <SideBar drawerWidth={ drawerWidth } />
 
             <Box 
                 component='main' 
                 sx={{ flexGrow: 1, p: 3 }}
             >
-                {/**Toolbar */}
+                <Toolbar />
 
                 {children}
 
